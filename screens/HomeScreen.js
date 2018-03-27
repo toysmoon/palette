9ffd9e744c3bf250bb9d3b713ab1e3957c5c0e1c
조성정 <joseongjeong@joseongjeong-ui-iMac.local>
2018-03-28 00:37:28 +0900

    first commit

---
+++ screens/HomeScreen.js
@@ -0,0 +1,22 @@
+import React from "react";
+import {
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View
+} from "react-native";
+
+export default class HomeScreen extends React.Component {
+  static navigationOptions = {
+    header: null
+  };
+
+  render() {
+    return <View />;
+  }
+}
+
+const styles = StyleSheet.create({});
